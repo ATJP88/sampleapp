dba8bc3de400acf9ba616e4ee1cb79bffc4fa556
ATJP88 <93024157+ATJP88@users.noreply.github.com>
2023-11-02 22:26:36 +0530

    Update deploy.sh

--- yaml_files/deploy.sh
+++ yaml_files/deploy.sh
@@ -1,4 +1,4 @@
 kubectl apply -f ./yaml_files/index-web-deploy.yaml
-kubectl apply -f ./yaml_files/service.yaml
+kubectl apply -f ./yaml_files/index-web-service.yaml
 kubectl apply -f ./yaml_files/index-web-ingress.yaml
 
