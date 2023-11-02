b03a5cbcaf8e5f507dd9f006e4802df8242c2d02
ATJP88 <93024157+ATJP88@users.noreply.github.com>
2023-11-02 22:19:25 +0530

    Update deploy.sh

--- yaml_files/deploy.sh
+++ yaml_files/deploy.sh
@@ -1,3 +1,3 @@
-kubectl apply -f index-web-deploy.yaml
-kubectl apply -f index-web-ingress.yaml
-kubectl apply -f service.yaml
+kubectl apply -f ./yaml_files/index-web-deploy.yaml
+kubectl apply -f ./yaml_files/index-web-ingress.yaml
+kubectl apply -f ./yaml_files/service.yaml
